3b19eaafc2947bbf9e9252f506affee32eec7a58
kokojj <kovjscl976@naver.com>
2024-06-15 00:24:22 +0900

    헤더생성

---
+++ researchlab/src/components/common/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header>
+            <img src=""></img>
+            <h1>ReserchLab</h1>
+            <div>메뉴</div>
+            <div>로그인 / 회원가입</div>
+        </header>
+    );
+}
