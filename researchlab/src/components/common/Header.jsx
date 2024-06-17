745d015d4965cb75d2c8100e372261be39090bf7
kokojj <kovjscl976@naver.com>
2024-06-17 22:51:39 +0900

    헤더 구조 잡기

--- researchlab/src/components/common/Header.jsx
+++ researchlab/src/components/common/Header.jsx
@@ -1,10 +1,23 @@
-export default function Header(){
+import './Header.css';
+
+export default function Header() {
+
+
+
     return (
         <header>
-            <img src=""></img>
-            <h1>ReserchLab</h1>
-            <div>메뉴</div>
-            <div>로그인 / 회원가입</div>
+                <h1 className="logo">ReserchLab</h1>
+                <div className='menuList'>
+                    <ul>
+                        <li>주요 뉴스</li>
+                        <li>종목 조회</li>
+                        <li>복리 계산기</li>
+                    </ul>
+                </div>
+                <div id='memberServiceArea'>
+                    <button>로그인</button>
+                    <button>회원가입</button>
+                </div>
         </header>
     );
 }
