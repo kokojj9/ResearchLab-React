66fa88fade8a0478c4f914d94382e5cad76577af
kokojj <kovjscl976@naver.com>
2024-06-22 19:53:16 +0900

    메뉴 컨텐츠 영역 생성 / 트레이딩뷰 위젯 테스트

--- researchlab/src/App.js
+++ researchlab/src/App.js
@@ -1,11 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
-import axios from "axios";
+import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './App.css';
+import axios from "axios";
 import Header from './components/common/Header.jsx'
-import LoginModal from './components/member/LoginModal.jsx';
-import EnrollModal from './components/member/EnrollModal.jsx';
-
-
+import MainNews from './components/contents/MainNews.jsx';
+import StockList from './components/contents/StockList.jsx';
 
 function App() {
   const [member, setMember] = useState(null);
@@ -20,53 +20,28 @@ function App() {
       url: '/members/getSession',
       withCredentials: true
     }).then(response => {
-      if(response.data != null){
-       setMember(response.data.data);
+      if (response.data != null) {
+        setMember(response.data.data);
       }
     })
   }
 
-  const loginDialog = useRef();
-  const EnrollDialog = useRef();
-
-  const openLoginModal = () => {
-    loginDialog.current.open();
-  }
-
-  const openEnrollModal = () => {
-    EnrollDialog.current.open();
-  }
-
-  const closeModal = () => {
-    loginDialog.current.close();
-    EnrollDialog.current.close();
-  }
-
-  const handleLogout = () => {
-    axios({
-      method : 'post',
-      url : '/members/logout',
-      withCredentials: true
-    }).then(response => {
-      setMember(null);
-    }).catch(response => {
-
-    })
-  }
-
   return (
-
-    <div className="App">
-      <Header
-        openLoginModal={openLoginModal}
-        openEnrollModal={openEnrollModal}
-        loginMember={member}
-        onLogout={handleLogout}
-      />
-      <LoginModal ref={loginDialog} closeModal={closeModal} getSession={getSession} ></LoginModal>
-      <EnrollModal ref={EnrollDialog} closeModal={closeModal}></EnrollModal>
-    </div>
-
+    <Router>
+      <div className="App">
+        <Header
+          loginMember={member}
+          getSession={getSession}
+          setMember={setMember}
+        />
+        <div className="content">
+          <Routes>
+            <Route path="/mainNews" element={<MainNews />} />
+            <Route path="/stockList" element={<StockList />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- researchlab/src/components/common/Header.jsx
+++ researchlab/src/components/common/Header.jsx
@@ -1,7 +1,42 @@
-import Button from '../member/Button.jsx';
-import './Header.css';
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Header({openLoginModal, openEnrollModal, loginMember, onLogout}) {
+import axios from 'axios';
+
+import './Header.css';
+import Button from '../member/Button.jsx';
+import LoginModal from '../member/LoginModal.jsx';
+import EnrollModal from '../member/EnrollModal.jsx';
+
+export default function Header({ loginMember, getSession, setMember }) {
+
+    const loginDialog = useRef();
+    const EnrollDialog = useRef();
+
+    const openLoginModal = () => {
+        loginDialog.current.open();
+    }
+
+    const openEnrollModal = () => {
+        EnrollDialog.current.open();
+    }
+
+    const closeModal = () => {
+        loginDialog.current.close();
+        EnrollDialog.current.close();
+    }
+
+    const handleLogout = () => {
+        axios({
+            method: 'post',
+            url: '/members/logout',
+            withCredentials: true
+        }).then(response => {
+            setMember(null);
+        }).catch(response => {
+
+        })
+    }
 
     return (
         <>
@@ -9,16 +44,16 @@ export default function Header({openLoginModal, openEnrollModal, loginMember, on
                 <h1 className="logo">ReserchLab</h1>
                 <div className='menuList'>
                     <ul>
-                        <li>주요 뉴스</li>
-                        <li>종목 조회</li>
-                        <li>복리 계산기</li>
+                        <li><Link className='menuLink' to="/mainNews">주요 뉴스</Link></li>
+                        <li><Link className='menuLink' to="/stockList">종목 조회</Link></li>
+                        <li><Link className='menuLink' to="/cal">복리 계산기</Link></li>
                     </ul>
                 </div>
                 <div id='memberServiceArea'>
                     {loginMember !== null ? (
                         <>
                             <p>{loginMember.memberId}님 반갑습니다.</p>
-                            <Button onSelect={onLogout}>로그아웃</Button>
+                            <Button onSelect={handleLogout}>로그아웃</Button>
                         </>
                     ) : (
                         <>
@@ -28,6 +63,8 @@ export default function Header({openLoginModal, openEnrollModal, loginMember, on
                     )}
                 </div>
             </header>
+            <LoginModal ref={loginDialog} closeModal={closeModal} getSession={getSession} ></LoginModal>
+            <EnrollModal ref={EnrollDialog} closeModal={closeModal}></EnrollModal>
         </>
     );
 }
