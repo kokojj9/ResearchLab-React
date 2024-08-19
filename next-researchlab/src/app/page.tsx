5f8ea0421b4ae66ba892264cd1867254039a25d0
kokojj <kovjscl976@naver.com>
2024-08-19 23:49:33 +0900

    [ 리팩토링 ] 메인 페이지 헤더 리팩토링 작업

--- next-researchlab/src/app/page.tsx
+++ next-researchlab/src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export default function Home() {
+// import { MemberProvider } from "./context/MemberContext.js";
+
+// import MainNews from "./pages/news/MainNews.js";
+// import StockList from "./components/contents/StockList.js";
+// import CryptoList from "./pages/investment/CryptoList.js";
+// import MainContent from "./pages/common/MainContent.js";
+// import ErrorPage from "./pages/common/error/ErrorPage.js";
+// import RootLayout from "./pages/common/Root.js";
+// import TradeDiaryBoard from "./pages/tradeboard/TradeDiaryBoard.js";
+// import InterestCalculatorPage from "./pages/calculator/InterestCalculator.js";
+// import NewTradePost from "./pages/tradeboard/NewTradePost.js";
+
+import "./App.css";
+// import TradePostDetail from "./components/tradeBoard/TeadePostDetail.js";
+
+const router = createBrowserRouter([
+  {
+    // path: "/",
+    // element: <RootLayout />,
+    // errorElement: <ErrorPage />,
+    // children: [
+    //   { index: true, element: <MainContent /> },
+    //   { path: "mainNews", element: <MainNews /> },
+    //   { path: "stockList", element: <StockList /> },
+    //   { path: "cryptoList", element: <CryptoList /> },
+    //   { path: "calculator", element: <InterestCalculatorPage /> },
+    //   {
+    //     path: "tradeBoard",
+    //     element: <TradeDiaryBoard />,
+    //     children: [
+    //       { path: "new", element: <NewTradePost /> },
+    //       { path: ":boardNo", element: <TradePostDetail /> },
+    //     ],
+    //   },
+    //   ,
+    // ],
+  },
+]);
+
+function App() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <MemberProvider>
+      <RouterProvider router={router} />
+    </MemberProvider>
   );
 }
+
+export default App;
