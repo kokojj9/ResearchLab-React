f3089b636bf5b5f5b10cc2d44df631860ffac6a7
kokojj <kovjscl976@naver.com>
2024-10-04 21:55:44 +0900

    [ feat ] 조회수 기능 추가
    1. 게시글에 조회수 표시

---
+++ next-researchlab/src/app/calculator/page.tsx
@@ -0,0 +1,9 @@
+const Calculator = () => {
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>계산기 페이지(개발중)</h2>
+    </>
+  );
+};
+
+export default Calculator;
---
+++ next-researchlab/src/app/stock/page.tsx
@@ -0,0 +1,9 @@
+const Stock = () => {
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>주식 페이지(개발중)</h2>
+    </>
+  );
+};
+
+export default Stock;
